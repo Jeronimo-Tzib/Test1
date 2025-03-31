fb5834f9a427e79a92eca42c3146de127427eb6c
Flakk <flakk@gmail.com>
2025-03-30 23:49:49 -0600

    committed main, implementation & header

---
+++ binary.h
@@ -0,0 +1,16 @@
+#ifndef BINARY_H
+#define BINARY_H
+
+#include <iostream>
+
+class Binary{
+
+
+
+
+
+
+
+};
+
+#endif // BINARY_H
---
+++ main.cpp
@@ -0,0 +1,16 @@
+// Jeronimo Tzib
+// Filename: Test 1 Part A
+
+
+#include <iostream> 
+
+using namespace std;
+
+int main(){
+
+
+
+
+
+    return 0;
+}
