571846600cf3c00dc77df0624d248f00611096cc
Flakk <flakk@gmail.com>
2025-04-01 00:11:56 -0600

    defined public and private variables in header

--- binary.cpp
+++ binary.cpp
@@ -1,2 +1,17 @@
 #include "binary.h"
 
+// constructor
+
+Binary::Binary() : root(nullptr){}
+
+// destructor to free memory
+
+Binary::~Binary(){
+    destroyTree(root);
+}
+
+// insert functon (calls recursive helper)
+
+void Binary(int value){
+    root = insertRec(root, value);
+}
--- binary.h
+++ binary.h
@@ -1,11 +1,26 @@
 #ifndef BINARY_H
 #define BINARY_H
 
+#include "node.h"
 #include <iostream>
 
 class Binary{
 
+public:
 
+    Node * root;
+
+    Binary();
+    ~Binary();
+
+    void insert(int value);
+    void inOrder();
+
+private:
+
+    Node* insertRec(Node* node, int value);
+    void inOrderRec(Node* node);
+    void destroyTree(Node* node);
 
 
 
